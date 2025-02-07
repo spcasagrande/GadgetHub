da047678fa0a6f81655545ed00002a7ea0c1e7e0
spcasagrande <146010636+spcasagrande@users.noreply.github.com>
2025-02-07 15:16:29 -0600

    Add project files.

---
+++ GadgetHub.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GadgetHub.Domain.Abstract;
+
+namespace GadgetHub.WebUI.Controllers
+{
+	public class ProductController : Controller
+	{
+		private IProductRepository myRepository;
+
+		public ProductController(IProductRepository productRepository)
+		{
+			this.myRepository = productRepository;
+		}
+
+		public ViewResult List()
+		{
+			return View(myRepository.Products);
+		}
+	}
+}
---
+++ GadgetHub.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Ninject;
+using Moq;
+using GadgetHub.Domain.Abstract;
+using GadgetHub.Domain.Entities;
+
+namespace GadgetHub.WebUI.Infrastructure
+{
+	public class NinjectDependencyResolver : IDependencyResolver
+	{
+		private IKernel mykernel;
+
+		public NinjectDependencyResolver(IKernel kernelParam)
+		{
+			mykernel = kernelParam;
+			AddBindings();
+		}
+
+		public object GetService(Type myserviceType)
+		{
+			return mykernel.TryGet(myserviceType);
+		}
+
+		public IEnumerable<object> GetServices(Type myserviceType)
+		{
+			return mykernel.GetAll(myserviceType);
+		}
+
+		private void AddBindings()
+		{
+			Mock<IProductRepository> myMock = new Mock<IProductRepository>();
+			myMock.Setup(m => m.Products).Returns(new List<Product> {
+				new Product{ Name = "Laptop", Brand = "ASUS", Price = 600, Description = "A portable computer", Category = "Computers" },
+				new Product{ Name = "IPhone", Brand = "Apple", Price = 1500, Description = "A smartphone", Category = "Phone" },
+				new Product{ Name = "TV", Brand = "LG", Price = 400, Description = "A smart TV that can access the internet", Category = "Entertainment" },
+			});
+			mykernel.Bind<IProductRepository>().ToConstant(myMock.Object);
+		}
+	}
+}
