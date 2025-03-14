0f8524aecd338cdc40475c3d23680acdb41735b7
spcasagrande <146010636+spcasagrande@users.noreply.github.com>
2025-03-14 18:03:47 -0500

    Lab 2

---
+++ GadgetHub.WebUI/Controllers/CartController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GadgetHub.Domain.Abstract;
+using GadgetHub.Domain.Entities;
+using GadgetHub.WebUI.Models;
+
+namespace GadgetHub.WebUI.Controllers
+{
+	public class CartController : Controller
+	{
+		private IProductRepository repository;
+
+		public CartController(IProductRepository repo)
+		{
+			repository = repo;
+		}
+
+		private Cart GetCart()
+		{
+			Cart cart = (Cart)Session["Cart"];
+
+			if (cart == null)
+			{
+				cart = new Cart();
+				Session["Cart"] = cart;
+			}
+
+			return cart;
+		}
+
+		// Add to cart
+		public RedirectToRouteResult AddToCart(Cart cart, int productID, string returnUrl)
+		{
+			Product product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
+
+			if (product != null)
+			{
+				GetCart().AddItem(product, 1);
+			}
+
+			return RedirectToAction("Index", new { returnUrl });
+		}
+
+		// Remove from cart
+		public RedirectToRouteResult RemoveFromCart(int productID, string returnUrl)
+		{
+			Product product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
+
+			if (product != null)
+			{
+				GetCart().RemoveLine(product);
+			}
+
+			return RedirectToAction("Index", new { returnUrl });
+		}
+
+		public ViewResult Index(string returnUrl)
+		{
+			return View(new CartIndexViewModel
+			{
+				Cart = GetCart(),
+				ReturnUrl = returnUrl
+			});
+		}
+	}
+}
---
+++ GadgetHub.WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GadgetHub.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GadgetHub.WebUI.Models
+{
+	public class CartIndexViewModel
+	{
+		public Cart Cart { get; set; }
+
+		public string ReturnUrl { get; set; }
+	}
+}
